0a4c8861d48b5106e450974453de41e5c7ee15d2
kinfe mulugeta <Kinfe2534@gmail.com>
2022-02-14 23:00:35 +0300

    dev tools extenison insital commit

---
+++ Chick/Chick/devtools.js
@@ -0,0 +1,11 @@
+// Can use
+// chrome.devtools.*
+// chrome.extension.*
+
+// Create a tab in the devtools area
+chrome.devtools.panels.create("Magpie Hatchery", 
+  "images/icon-brand_prominence-noshadow.png", 
+  "panel.html", 
+  function(panel) {
+
+  });
